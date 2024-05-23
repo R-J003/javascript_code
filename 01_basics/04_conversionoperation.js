3c928eb9edbe9d23ddb0abdfd314beaeb3db47ec
R-J003 <rudrajain2003@gmail.com>
2024-05-23 18:48:23 +0530

    comparison of data types

--- 01_basics/04_conversionoperation.js
+++ 01_basics/04_conversionoperation.js
@@ -3,13 +3,13 @@
 // console.log(typeof(score));
 // console.log(typeof score);
 
-let score = "33avaa"
-console.log(score);
-console.log(typeof(score));
+// let score = "33avaa"
+// console.log(score);
+// console.log(typeof(score));
 
-let value = Number(score)
-console.log(typeof value);
-console.log(value);
+// let value = Number(score)
+// console.log(typeof value);
+// console.log(value);
 
 // "33" --> 33
 // "33abc" --> NaN
@@ -25,3 +25,44 @@ console.log(booleanIsLoggedIn)
 // 0 --> false;
 // "" --> false;
 // "string" --> true;
+
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
