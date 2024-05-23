fb479b1eeaae6779b445fce549e0183c74655884
R-J003 <rudrajain2003@gmail.com>
2024-05-23 14:08:56 +0530

    message"conversion of data types"

---
+++ 01_basics/04_conversionoperation.js
@@ -0,0 +1,27 @@
+// let score = 33
+// console.log(score);
+// console.log(typeof(score));
+// console.log(typeof score);
+
+let score = "33avaa"
+console.log(score);
+console.log(typeof(score));
+
+let value = Number(score)
+console.log(typeof value);
+console.log(value);
+
+// "33" --> 33
+// "33abc" --> NaN
+// true --> 1; false --> 0;
+
+
+let isLoggedIn = "rudra" //1, 0, ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// 1 --> true;
+// 0 --> false;
+// "" --> false;
+// "string" --> true;
